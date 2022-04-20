8c9b7efcfdc49af6a22242ecc253c2139c22bb03
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2022-04-20 20:55:34 -0300

    feat: add unit test workflow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Button } from "./components/Button";
 
 function App() {
-  return <Button>Download</Button>;
+  return <Button data-testid="download-button">Download</Button>;
 }
 
 export default App;
