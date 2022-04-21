6a5016a0c7c7d93d9551d7b601829cf1c541406c
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2022-04-20 21:06:14 -0300

    test: make the test fail

--- src/components/Button/index.spec.jsx
+++ src/components/Button/index.spec.jsx
@@ -5,6 +5,6 @@ describe("Button test", () => {
   it("should render correctly", () => {
     render(<Button data-testid="download-button">Download</Button>);
 
-    expect(screen.getByTestId("download-button")).toBeInTheDocument();
+    expect(screen.queryByTestId("download-button")).not.toBeInTheDocument();
   });
 });
